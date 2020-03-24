e65231772638d6849f12cbb40a4c1c94afe19f4b
zerolhao <zerolhao@gmail.com>
2020-03-24 14:28:47 +0800

    添加代码格式化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    //强制使用单引号
+    // quotes: ['error', 'single'],
+    //强制不使用分号结尾
+    // semi: ['error', 'never']
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   }
-};
+}
